b1cc7bbf07da36f011c6a8223509c8e1f592cb25
said <baichm60@gmail.com>
2023-05-03 11:31:38 +0100

    'modified'

---
+++ database/migrations/2023_05_03_095643_create_profs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profs', function (Blueprint $table) {
+            $table->id();
+            $table->string('cni')->unique();
+            $table->string('ppr')->unique();
+            $table->string('f_name');
+            $table->string('l_name');
+            $table->foreignId('school_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profs');
+    }
+};
---
+++ database/migrations/2023_05_03_100419_create_absences_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('absences', function (Blueprint $table) {
+            $table->id();
+            $table->date('start');
+            $table->date('end');
+            $table->enum('motif',['justified , non_justified'])->default('justified');
+            $table->enum('justification',['medical_certificate , Exception license']);
+            $table->foreignId('prof_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('absences');
+    }
+};
